3b72794c41c5df93637cc675d9886a102c0fbadc
Alper Tekin <alper.tekin@ug.bilkent.edu.tr>
2026-01-28 10:30:12 +0300

    part a
    create array

---
+++ arrayCheck.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class arrayCheck {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int arraySize = input.nextInt()
+        input.nextLine();
+        int[] randomArray = new int[arraySize];
+
+        for(int i = 0; i < arraySize; i++) {
+            randomArray[i] = Math.floor(Math.random() * 101)
+        }
+    }
+}
